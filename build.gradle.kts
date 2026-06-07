46902ec1c1ce2611296c3193f06f1687670b7940
ihateblueb <ihateblueb@proton.me>
2026-06-07 15:08:52 -0400

    Specify Java 21 in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,12 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
 }
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
 tasks {
     build {
         dependsOn(processResources)
